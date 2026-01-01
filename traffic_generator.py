9dc63d88abcdc3a85711c1ffd078b7e69c340ab5
Vincenzo Pelosio <v.pelosio@gmail.com>
2026-01-01 12:45:35 +0100

    Traffic generator WIP

---
+++ traffic_generator.py
@@ -0,0 +1,108 @@
+import random
+import numpy as np
+from enum import Enum
+
+# given an episode i, here we decide the scenario and generate the vehicles
+
+class Scenario(Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+    UNBALANCED = "unbalanced"
+    WAVE = "wave"
+
+
+class TrafficGenerator:
+    def __init__(self, sim_config):
+        self.sim_config = sim_config
+
+        self.scenario_probs = {
+            Scenario.LOW:        0.05,
+            Scenario.MEDIUM:     0.25,
+            Scenario.HIGH:       0.35,
+            Scenario.UNBALANCED: 0.20,
+            Scenario.WAVE:       0.15
+        }
+    
+    # Private Methods
+    def _get_vehicle_count(self, scenario: Scenario):
+        if scenario == Scenario.LOW:
+            val = np.random.normal(600, 150)
+            return int(np.clip(val, 250, 900))
+        
+        elif scenario == Scenario.MEDIUM:
+            val = np.random.normal(1400, 300)
+            return int(np.clip(val, 900, 2000))
+        
+        elif scenario == Scenario.HIGH:
+            val = np.random.normal(2800, 400)
+            return int(np.clip(val, 2000, 3200))
+            
+        elif scenario == Scenario.UNBALANCED:
+            val = np.random.normal(2000, 300)
+            return int(np.clip(val, 1500, 2500))
+            
+        elif scenario == Scenario.WAVE:
+            val = np.random.normal(2600, 300)
+            return int(np.clip(val, 2200, 3000))
+            
+        return 1000 
+    
+    def _get_depart_times(self, n, scenario: Scenario):
+        duration = 3600
+
+        if scenario == Scenario.WAVE:
+            # 10% (15m) -> 80% (30m) -> 10% (15m)
+            n_peak = int(n*0.80)
+            n_rest = n - n_peak
+            n_start = int(n_rest/2)
+            n_end = n_rest - n_start # in case they are not even
+
+            t1 = np.random.uniform(0, 900, n_start)
+            t2 = np.random.uniform(900, 2700, n_peak)
+            t3 = np.random.uniform(2700, duration, n_end)
+
+            times = np.concatenate([t1, t2, t3])
+        else:
+            times = np.random.uniform(0, duration, n)
+
+        return times
+    
+    def _calculate_route_weights(self, scenario: Scenario):
+        weights = {rid: 0.0 for rid in self.sim_config.route_ids}
+
+        def set_w(group, val):
+            if group in self.sim_config.routes_map:
+                for r in self.sim_config.routes_map[group]:
+                    weights[r] = val
+
+        # Case 1: Standard (Low, Med, Wave) -> 70% Straight, 15% Turn
+        if scenario in [Scenario.LOW, Scenario.MEDIUM, Scenario.WAVE]:
+            for prefix in ["NS", "EW"]:
+                set_w(f"{prefix}_Straight", 70.0)
+                set_w(f"{prefix}_Right", 15.0)
+                set_w(f"{prefix}_Left", 15.0)
+        
+        # Case 2: High -> 85% Straight, 10% dx, 5% sx (to minimize starvation for left turn)
+        elif scenario == Scenario.HIGH:
+            for prefix in ["NS", "EW"]:
+                set_w(f"{prefix}_Straight", 85.0)
+                set_w(f"{prefix}_Right", 10.0)
+                set_w(f"{prefix}_Left", 5.0)
+
+        # Case 3: Unbalanced -> N-S dominates. Main road during rush hour
+        elif scenario == Scenario.UNBALANCED:
+            vol_ns = 9.0
+            vol_ew = 1.0
+
+            set_w('NS_Straight', 85.0 * vol_ns)
+            set_w('NS_Right', 10.0 * vol_ns)
+            set_w('NS_Left', 5.0 * vol_ns)
+            
+            set_w('EW_Straight', 70.0 * vol_ew)
+            set_w('EW_Right', 15.0 * vol_ew)
+            set_w('EW_Left', 15.0 * vol_ew)
+        
+        return [weights[rid] for rid in self.sim_config.route_ids]
+
+
